8b17bd3f80cc5bcae9f4f9b27730eaf56cf0e4d9
tvatt <github@vattima.com>
2024-11-29 19:53:56 -0500

    Generalizing github action for snapshot, dev and prod.

--- src/main/java/com/vattima/lego/data/service/controller/ItemInventoryController.java
+++ src/main/java/com/vattima/lego/data/service/controller/ItemInventoryController.java
@@ -5,6 +5,7 @@ import net.lego.data.v2.dao.ItemInventoryDao;
 import net.lego.data.v2.dto.ItemInventory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,15 @@ public class ItemInventoryController {
     @GetMapping
     public ResponseEntity<List<ItemInventory>> findAll() {
         return ResponseEntity.ok(itemInventoryDao.findAll());
+    }
+
+    @GetMapping("/uuid/{uuid}")
+    public ResponseEntity<ItemInventory> findByUuid(@PathVariable final String uuid) {
+        return itemInventoryDao.findByUuid(uuid).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{itemInventoryId}")
+    public ResponseEntity<ItemInventory> findByUuid(@PathVariable final Integer itemInventoryId) {
+        return itemInventoryDao.findByItemInventoryId(itemInventoryId).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
